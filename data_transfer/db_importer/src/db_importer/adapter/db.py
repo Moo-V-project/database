fcb0f02101c8163e6f94324a90f6db22ee0bc6e6
almiluk <almiluk@gmail.com>
2025-12-17 18:33:49 +0200

    db_importer: add read all csvs function

---
+++ data_transfer/db_importer/src/db_importer/adapter/db.py
@@ -0,0 +1,3 @@
+# TODO: implement function connecting to the DB and returning a cursor object
+# TODO: implement functions for upserting data from each data frame,
+#       the functions should take a data frame ans a DB cursor as arguments
