058e41f55b0546cd2bc746905deb37b8aff950d5
almiluk <almiluk@gmail.com>
2025-12-19 13:35:20 +0200

    tests: add tests for csv loading function

--- data_transfer/db_importer/test/unit/adapter/test_csv.py
+++ data_transfer/db_importer/test/unit/adapter/test_csv.py
@@ -1,7 +1,20 @@
+import pytest
+from jsonschema import ValidationError
+
+
 from db_importer.adapter.csv import read_all_movie_csvs
 
 
-def test_read_all_movie_csvs(csv_fixtures_dir):
-    genres_df, countries_df, companies_df, people_df, movies_df = (
-        read_all_movie_csvs(base_path=csv_fixtures_dir / "valid")
-    )
+def test_read_csvs_valid(csv_dir_valid):
+    dfs = read_all_movie_csvs(csv_dir_valid)
+    assert len(dfs) == 5
+
+
+def test_read_csvs_missing_file(csv_dir_missing_file):
+    with pytest.raises(FileNotFoundError):
+        read_all_movie_csvs(csv_dir_missing_file)
+
+
+def test_read_csvs_invalid(csv_dir_invalid):
+    with pytest.raises((ValueError, ValidationError)) as e_info:
+        read_all_movie_csvs(csv_dir_invalid)
