8be903f5b7275ccd7e1307bd2d858dfa185bf352
almiluk <almiluk@gmail.com>
2025-12-19 13:35:20 +0200

    tests: add tests for csv loading function

--- data_transfer/db_importer/test/unit/conftest.py
+++ data_transfer/db_importer/test/unit/conftest.py
@@ -1,7 +1,22 @@
 from pathlib import Path
+from glob import glob
+
 import pytest
 
 
+BASE_CSV_PATH = Path(__file__).parent.parent / "fixtures" / "csv"
+
+
+@pytest.fixture(params=glob(f"{BASE_CSV_PATH}/invalid/*"))
+def csv_dir_invalid(request) -> Path:
+    return Path(request.param)
+
+
 @pytest.fixture
-def csv_fixtures_dir() -> Path:
-    return Path(__file__).parent.parent / "fixtures" / "csv"
+def csv_dir_valid() -> Path:
+    return BASE_CSV_PATH / "valid"
+
+
+@pytest.fixture
+def csv_dir_missing_file() -> Path:
+    return BASE_CSV_PATH / "missing_file"
